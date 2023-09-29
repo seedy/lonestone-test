5c91b2ea7bbafeb19a552fbb11c925dbac586b3c
Cédric <seedy.dupuis@gmail.com>
2023-09-29 16:30:24 +0200

    fix(Button): layout

--- components/ButtonChifumi/index.tsx
+++ components/ButtonChifumi/index.tsx
@@ -1,19 +1,24 @@
 import { Move, PAPER, ROCK, SCISSOR } from "@/constants/move"
 import { ComponentProps } from "react"
-import {Button} from "@chakra-ui/react"
+import {Button, Icon} from "@chakra-ui/react"
 import styles from "./index.module.scss"
 import {FaHandRock, FaHandPaper, FaHandScissors} from "react-icons/fa";
+
+const asIcons = {
+    ROCK: FaHandRock,
+    PAPER: FaHandPaper,
+    SCISSOR: FaHandScissors,
+} as const
 
 interface ButtonChifumiProps extends ComponentProps<"button"> {
     variant: Move
 
 }
 const ButtonChifumi = ({variant, ...props}: ButtonChifumiProps) => {
+    
     return (
-  <Button className={styles.root} {...props}>
-    {variant === ROCK && <FaHandRock />}
-    {variant === PAPER && <FaHandPaper />}
-    {variant === SCISSOR && <FaHandScissors />}
+  <Button h="auto" gap={2} w="auto" p={8} className={styles.root} {...props}>
+    <Icon as={asIcons[variant]}/>
 
     {variant}
     </Button>
